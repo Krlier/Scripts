02407d3a9c482b7a22fc76e0773ed872a45d3a29
Krlier <daniel.carlier@hotmail.com>
2019-01-12 14:34:19 -0200

    [FIX] Update my terminal configuration

--- terminal.sh
+++ terminal.sh
@@ -1,15 +1,33 @@
+# PS1="[\[$(tput sgr0)\]\[\033[38;5;9m\]\A\[$(tput sgr0)\]\[\033[38;5;15m\]] ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@labs\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+
+function Color() {
+  echo "\[$(tput setaf $1)\]"
+}
+function ResetColor() {
+  echo "\[$(tput sgr0)\]"
+}
+
+function BashPrompt() {
+    local last_status=$?
+    local reset=$(ResetColor)
+
+    local failure="✘"
+    local success="✔"
+
+    if [[ "$last_status" != "0" ]]; then
+        last_status="$(Color 5)$failure$reset"
+    else
+        last_status="$(Color 2)$success$reset"
+    fi
+
+    echo "$last_status"
+}
+
+
+alias ls="ls -G"
+
 parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
-emoji() {
-        if [ $? == 0 ]
-        then
-                echo ✅
-        else
-                echo ❌
-        fi
-}
-
-export CLICOLOR=1
-export PS1="\$(emoji)\[\033[38;5;9m\][\A]\[$(tput sgr0)\]\[\033[38;5;10m\]\u@Labs\[$(tput sgr0)\]\[\033[38;5;12m\]:\w\[$(tput sgr0)\]\[\033[38;5;15m\]\$(parse_git_branch)\n\\$ \[$(tput sgr0)\]"
+PROMPT_COMMAND='PS1="$(BashPrompt) [\[$(tput sgr0)\]\[\033[38;5;9m\]\A\[$(tput sgr0)\]\[\033[38;5;15m\]] ${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@labs\[\033[00m\]:\[\033[01;34m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]\[\033[00m\]\n\$ "'
